9eb7e35d3a4e734c1e5341cf8f628260c7688ad8
trunghaiii <haitran022001@gmail.com>
2024-06-01 20:09:13 -0400

    hypotenuse calculator program

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace C_Programming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. hypotenuse calculator program
+            Console.WriteLine("Enter Side A");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter Side B");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double c = Math.Sqrt((a * a) + (b * b));
+
+            Console.WriteLine("The hypotenuse is " + c);
+        }
+    }
+}
